2cd601f5d330c06f937a946bb5c35733d0965011
novabimperm-creator <novabimperm@gmail.com>
2026-05-12 14:19:46 +0500

    asynchelper added

---
+++ AsyncHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TNovCommon
+{
+    public static class AsyncHelper
+    {
+        // Запускает асинхронную операцию без UI-контекста и синхронно ожидает завершения
+        public static T RunSync<T>(Func<Task<T>> asyncFunc)
+        {
+            return Task.Run(async () => await asyncFunc()).GetAwaiter().GetResult();
+        }
+
+        // Перегрузка для операций без результата (сохранение, логирование и т.п.)
+        public static void RunSync(Func<Task> asyncFunc)
+        {
+            Task.Run(async () => await asyncFunc()).GetAwaiter().GetResult();
+        }
+    }
+}

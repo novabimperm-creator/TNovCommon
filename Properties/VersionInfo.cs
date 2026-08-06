5761c73a77d091c35eeb999415bd394d815c9ba3
novabimperm-creator <roshior.a@pm-nova.ru>
2026-08-06 09:19:27 +0500

    tech

--- Properties/VersionInfo.cs
+++ Properties/VersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.26.0715.939")]
-[assembly: AssemblyFileVersion("2.26.0715.939")]
+[assembly: AssemblyVersion("2.26.0804.947")]
+[assembly: AssemblyFileVersion("2.26.0804.947")]
